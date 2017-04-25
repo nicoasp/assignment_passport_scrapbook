83e1d4001eba3ad2a8cf50060997097dd08ab542
nicoasp <nicolas.amaya@gmail.com>
2017-04-25 21:39:18 +0200

    change facebook strategy

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,8 +1,9 @@
-function connectModule(user, moduleName) {
+function connectModule(user, accessToken, moduleName) {
 	let index = user.passportModules.findIndex((module) => {
 		return module.name === moduleName;
 	})
 	user.passportModules[index].connected = true;
+	user.passportModules[index].token = accessToken;
 }
 
 
--- strategies/facebook.js
+++ strategies/facebook.js
@@ -13,24 +13,36 @@ const facebookStrategy = () => {
     function(accessToken, refreshToken, profile, done) {
       const facebookId = profile.id;
       const displayName = profile.displayName;
-
-      console.log(profile);
-      User.findOne({ facebookId }, function(err, user) {
-        if (err) return done(err);
-
-        if (!user) {
-          // Create a new account if one doesn't exist
-          user = new User({ facebookId, displayName });
-          connectModule(user, "Facebook");
-          user.save((err, user) => {
-            if (err) return done(err);
+      
+      if (req.user) {
+        req.user.facebookId = facebookId;
+        req.user.displayName = displayName;
+        connectModule(user, accessToken, "Facebook");
+        req.user.save((err, user) => {
+          if (err) {
+            done(err);
+          } else {
             done(null, user);
-          });
-        } else {
-          // Otherwise, return the extant user.
-          done(null, user);
-        }
-      });
+          }
+        });
+      } else {
+        User.findOne({ facebookId }, function(err, user) {
+          if (err) return done(err);
+
+          if (!user) {
+            // Create a new account if one doesn't exist
+            user = new User({ facebookId, displayName });
+            connectModule(user, accessToken, "Facebook");
+            user.save((err, user) => {
+              if (err) return done(err);
+              done(null, user);
+            });
+          } else {
+            // Otherwise, return the extant user.
+            done(null, user);
+          }
+        });
+      }
     }
   )
 }
